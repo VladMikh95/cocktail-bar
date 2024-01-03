1c8195b4705806a50b8a58e2eebed1eb26ee2e37
Vlad Mikhnenok <vlad.mikhnenok.95@gmail.com>
2024-01-03 22:11:42 +0300

    Сделан дизайн CocktailDetailFragment

--- app/src/main/java/ml/vladmikh/projects/cocktail_bar/MainActivity.kt
+++ app/src/main/java/ml/vladmikh/projects/cocktail_bar/MainActivity.kt
@@ -1,9 +1,13 @@
 package ml.vladmikh.projects.cocktail_bar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import ml.vladmikh.projects.cocktail_bar.databinding.ActivityMainBinding
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val window = window;
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
     }
 }
